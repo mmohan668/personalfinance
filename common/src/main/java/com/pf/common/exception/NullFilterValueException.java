2b100848f2f012d3cd4a9da1c3fd86566ce326b0
Mohan Reddy Mutra <mmohan668@gmail.com>
2026-08-31 08:38:59 +0530

    Custom Exceptions added

---
+++ common/src/main/java/com/pf/common/exception/NullFilterValueException.java
@@ -0,0 +1,7 @@
+package com.pf.common.exception;
+
+public class NullFilterValueException extends RuntimeException{
+    public NullFilterValueException(String message) {
+        super(message);
+    }
+}
